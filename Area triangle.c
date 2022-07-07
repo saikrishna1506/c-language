08d3e0b8dbdb4fe4867b326b40f9bd55f9821a95
saikrishna1506 <108617880+saikrishna1506@users.noreply.github.com>
2022-07-07 17:59:54 +0530

    Update Area triangle.c

--- Area triangle.c
+++ Area triangle.c
@@ -3,7 +3,7 @@
 int main()
 {
     int side1,side2;
-    printf("Enter the length of the sides:");
+    printf("Enter the length of the sides:")
     scanf("%d%d",&side1,&side2);
     float area=side1*side2/2;
     printf("The area of triangle is:%f",area);
